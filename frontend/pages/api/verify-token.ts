9622c5604a33f602a36db9bd2c9ab7070b021c68
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-02-05 14:03:34 +0900

    Implement token verification API endpoint with Privy authentication and message signing

---
+++ frontend/pages/api/verify-token.ts
@@ -0,0 +1,66 @@
+import { PrivyClient } from "@privy-io/server-auth";
+import { encodePacked, keccak256 } from "viem";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end(); // メソッドがPOSTでない場合は405エラー
+  }
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ error: "Authorization header missing" });
+  }
+
+  const privy = new PrivyClient(
+    process.env.NEXT_PUBLIC_PRIVY_APP_ID || "",
+    process.env.PRIVY_SECRET_ID || ""
+  );
+
+  try {
+    const verifiedClaims = await privy.verifyAuthToken(authHeader);
+    console.log("verifiedClaims", verifiedClaims);
+
+    // const {id, address, chainType} = await privy.walletApi.create({chainType: 'ethereum'});
+    // console.log("id", id);
+    // console.log("address", address);
+    // console.log("chainType", chainType);
+
+    const user = await privy.getUserById(verifiedClaims.userId);
+    console.log("user.github.username", user?.github?.username);
+    console.log("user.wallet.address:", user?.wallet?.address);
+
+    const messageHash = keccak256(
+      encodePacked(
+        ["string", "address"],
+        [
+          user?.github?.username || "",
+          (user?.wallet?.address || "0x0") as `0x${string}`,
+        ]
+      )
+    );
+    const ethSignedMessageHash = keccak256(
+      encodePacked(
+        ["string", "bytes32"],
+        ["\x19Ethereum Signed Message:\n32", messageHash]
+      )
+    );
+
+    const response = await privy.walletApi.ethereum.signMessage({
+      walletId: process.env.PRIVY_WALLET_ID || "",
+      message: ethSignedMessageHash,
+    });
+
+    const { signature, encoding } = response;
+
+    // トークンが有効な場合の処理
+    res.status(200).json({
+      message: "Token is valid",
+      user: verifiedClaims,
+      signature: signature,
+      encoding: encoding,
+    });
+  } catch (error) {
+    console.error(`Token verification failed with error ${error}.`);
+    res.status(401).json({ error: "Invalid token" });
+  }
+}
