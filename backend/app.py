e93a2a4e10ef9edc82c8eab645d1a7b92f618ed6
kimura-eagency <yuki.kimura.77777@gmail.com>
2025-02-09 21:58:43 +0900

    update cors

--- backend/app.py
+++ backend/app.py
@@ -24,6 +24,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 CORS(app, resources={r"/*": {"origins": "http://localhost:3001"}})
 CORS(app, resources={r"/*": {"origins": "http://localhost:3002"}})
+CORS(app, resources={r"/*": {"origins": "https://frontend-991346403603.asia-east2.run.app"}})
 
 # Setup SQLite tables
 setup()
