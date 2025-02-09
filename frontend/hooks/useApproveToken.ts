0f646709e7209441ce58e3565ee940ba1cd74746
naizo10 <76167545+naizo01@users.noreply.github.com>
2025-02-09 13:51:28 +0900

    Add token approval flow in SignButton component

--- frontend/hooks/useApproveToken.ts
+++ frontend/hooks/useApproveToken.ts
@@ -2,33 +2,41 @@ import { useState } from "react";
 import {
   useWriteContract,
   useWaitForTransactionReceipt,
+  useReadContract,
+  useAccount,
 } from "wagmi";
-import { erc20Abi } from 'viem'
-import { TOKEN_ADDRESS } from "@/constants/config";
+import { erc20Abi } from "viem";
+import { TOKEN_ADDRESS, CONTRACT_ADDRESS } from "@/constants/config";
 
 export function useApproveToken() {
   const [error, setError] = useState<Error | null>(null);
+  const { address } = useAccount();
 
-  const {
-    writeContract: write,
-    data: hash,
-    isPending,
-  } = useWriteContract();
+  const { writeContract: write, data: hash, isPending } = useWriteContract();
 
   const { isLoading: isConfirming } = useWaitForTransactionReceipt({
     hash,
   });
 
-  const approveToken = async (
-    spenderAddress: `0x${string}`,
-    amount: bigint
-  ) => {
+  const { data: allowance, refetch: fetchAllowance } = useReadContract({
+    address: TOKEN_ADDRESS,
+    abi: erc20Abi,
+    functionName: "allowance",
+    args: [address as `0x${string}`, CONTRACT_ADDRESS as `0x${string}`],
+  });
+
+  const approveToken = async () => {
     try {
       await write({
         address: TOKEN_ADDRESS,
         abi: erc20Abi,
         functionName: "approve",
-        args: [spenderAddress, amount],
+        args: [
+          CONTRACT_ADDRESS,
+          BigInt(
+            11579208923731619542357098500868790785326998466564056403945758400791312963995
+          ),
+        ],
       });
     } catch (error) {
       console.error("Error approving token:", error);
@@ -39,9 +47,11 @@ export function useApproveToken() {
 
   return {
     approveToken,
+    fetchAllowance,
+    allowance,
     isPending,
     isConfirming,
     error,
     transactionHash: hash,
   };
-} 
+}
